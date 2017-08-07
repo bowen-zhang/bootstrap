a1bc16995fdd4f30c32ea24cab9d29356e631cac
Bowen Zhang <bowenzhang1128@gmail.com>
2017-08-06 23:30:58 -0700

    update wifi scripts

--- switch_to_ap.sh
+++ switch_to_ap.sh
@@ -3,7 +3,4 @@
 sudo cp ./interfaces.ap /etc/network/interfaces
 sudo systemctl enable hostapd
 sudo systemctl enable dnsmasq
-sudo service hostapd start
-sudo service dnsmasq start
-sudo ifconfig wlan0 down
-sudo ifconfig wlan0 up
+sudo reboot
--- switch_to_wifi.sh
+++ switch_to_wifi.sh
@@ -5,7 +5,4 @@ sudo cp ./wpa_supplicant.conf.atc /etc/wpa_supplicant/wpa_supplicant.conf
 
 sudo systemctl disable hostapd
 sudo systemctl disable dnsmasq
-sudo service hostapd stop
-sudo service dnsmasq stop
-sudo ifconfig wlan0 down
-sudo ifconfig wlan0 up
+sudo reboot
